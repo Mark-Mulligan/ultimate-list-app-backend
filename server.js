9c2f9bfa723abe0191bcd7154a1fbabd23ec11aa
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-08 14:40:20 -0600

    connected to local instance of mongodb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const connectDB = require('./database/db');
 
 app.use(express.urlencoded({ extended: true, limit: '50mb ' }));
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`);
